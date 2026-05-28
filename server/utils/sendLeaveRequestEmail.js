498b3e064b5d4bc791e6dbf67fd62b6858daa031
Alain Bedetse <bedetsedavis@gmail.com>
2026-05-28 19:32:12 +0200

    Modified email SMTP

--- server/utils/sendLeaveRequestEmail.js
+++ server/utils/sendLeaveRequestEmail.js
@@ -7,10 +7,24 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  requireTLS: true,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("SMTP verify error:", error);
+  } else {
+    console.log("SMTP server ready", success);
+  }
 });
 
 const sendLeaveRequestEmail = async ({
