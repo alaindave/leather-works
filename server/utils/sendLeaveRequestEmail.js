9d2f9e435cc7baf8209acac56e42465c49301a78
Alain Bedetse <bedetsedavis@gmail.com>
2026-05-28 19:46:02 +0200

    Hardcoded gmail IPv4 host

--- server/utils/sendLeaveRequestEmail.js
+++ server/utils/sendLeaveRequestEmail.js
@@ -4,7 +4,7 @@ const dns = require("dns");
 dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "74.125.69.108",
   port: 587,
   secure: false,
   requireTLS: true,
